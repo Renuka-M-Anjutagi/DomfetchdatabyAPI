0a29f5a3bb68ef707bfd700497623fabc0b6fe2b
Renuka-M-Anjutagi <162134316+Renuka-M-Anjutagi@users.noreply.github.com>
2024-04-05 14:46:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,9 +23,12 @@ function foo(data1)
         var col = document.createElement("div")
         col.className = "col-md-4"
         col.innerHTML = `<div class="card" style="width: 18rem;">
+        <div class="card-header">
+        <h5 class="card-title">${data1[i].name.common}</h5>
+
+        </div>
         <img src="${data1[i].flags.png}" class="card-img-top" style="max-width: 540px;" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Countries List from API</h5>
           <ul class="list-group list-group-flush">
           <li class="list-group-item">Capital: ${data1[i].capital}</li>
           <li class="list-group-item"> Region :  ${data1[i].region}</li>
